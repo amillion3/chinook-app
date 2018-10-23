38393a401fae1196412e4e9e703826cf7e2ad0d1
Andy Million <amillion3@gmail.com>
2018-10-22 21:25:16 -0500

    setup in progress

---
+++ ChinookApp/DataAccess/DataStorage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChinookApp.Models;
+
+namespace ChinookApp.DataAccess
+{
+    public class DataStorage
+    {
+        static List <Invoice> _invoice = new List<Invoice>();
+        private const string ConnectionString = "Server=(local);Database=hihihihi;Trusted_Connection=True;";
+        
+        public bool addInvoice(Invoice invoice)
+        {
+            using (var db = new SqlConnection(ConnectionString)) 
+            {
+                return Ok();
+            }
+        }
+    }
+}
