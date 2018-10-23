3237c86f1510df809bdd9278e8ed575d9511df31
Andy Million <amillion3@gmail.com>
2018-10-22 20:43:02 -0500

    setup models

---
+++ ChinookApp/Models/Invoice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChinookApp.Models
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+        public int CustomerId { get; set; }
+        public string BillingAddress { get; set; }
+        public string BillingCity { get; set; }
+        public string BillingState { get; set; }
+        public string BillingCountry { get; set; }
+        public string BillingPostalCode { get; set; }
+    }
+}
