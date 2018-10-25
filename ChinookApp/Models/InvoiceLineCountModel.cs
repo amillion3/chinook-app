04eaaa85423a8af5dc3de6c85cdbb37676ac0e5f
Andy Million <amillion3@gmail.com>
2018-10-24 22:37:33 -0500

    WIP

---
+++ ChinookApp/Models/InvoiceLineCountModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChinookApp.Models
+{
+    // Looking at the InvoiceLine table, provide an endpoint that COUNTs the number 
+    //  of line items for an Invoice with a parameterized Id from user input.
+    // HINT, this could use `ExecuteScalar`
+    public class InvoiceLineCountModel
+    {
+        public int LineItemCount { get; set; }
+    }
+}
