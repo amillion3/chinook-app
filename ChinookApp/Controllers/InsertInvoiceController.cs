3d39111150a1c35dfa427e2e942cd64ed7d6b726
Andy Million <amillion3@gmail.com>
2018-10-25 22:17:47 -0500

    exercise 4 complete, POSt works!!!

---
+++ ChinookApp/Controllers/InsertInvoiceController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ChinookApp.DataAccess;
+
+
+namespace ChinookApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class InsertInvoiceController : ControllerBase
+    {
+        private readonly InsertInvoiceStorage _newInvoice;
+        public InsertInvoiceController()
+        {
+            _newInvoice = new InsertInvoiceStorage();
+        }
+        [HttpPost]
+        public IActionResult InsertInvoice(int id, DateTime date, string address, string city, string state, string country, string zip, decimal total)
+        {
+            return Ok(_newInvoice.InsertInvoice(id, date, address, city, state, country, zip, total));
+        }
+    }
+}
