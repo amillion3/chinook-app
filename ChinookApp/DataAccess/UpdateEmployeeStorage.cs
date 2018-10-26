1ce76421f8eaf066819623ca4fb1a6c305c9b0be
Andy Million <amillion3@gmail.com>
2018-10-25 22:27:19 -0500

    wip

---
+++ ChinookApp/DataAccess/UpdateEmployeeStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChinookApp.Models;
+
+namespace ChinookApp.DataAccess
+{
+    public class UpdateEmployeeStorage
+    {
+        private const string ConnectionString = "Server=(local);Database=Chinook;Trusted_Connection=True;";
+
+        // Provide a new endpoint to UPDATE an Employee's name with a 
+        //  parameter for Employee Id and new name
+    }
+}
---
+++ ChinookApp/Models/UpdateEmployeeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChinookApp.Models
+{
+    // Provide a new endpoint to UPDATE an Employee's name with a 
+    //  parameter for Employee Id and new name
+    public class UpdateEmployeeModel
+    {
+    }
+}
